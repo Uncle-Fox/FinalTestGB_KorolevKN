9f1d7925e22989983cebbec5b376bb32aac4bc7a
Uncle-Fox <Nemogemut@gmail.com>
2023-11-23 19:49:36 +0300

    добавили цикл, в кот.ищем эл-ты <=3 и add в newList

--- Final_Test_KorolevKN.cs
+++ Final_Test_KorolevKN.cs
@@ -1,3 +1,11 @@
 ﻿string[] list = { "2", "1567", "-2", "computer science", ":-)", "Greg", "want", "to", "Mars" };
 
 var newList = new List<string>();
+
+for (int i = 0; i < list.Length; i++)
+{
+    if (list[i].Length <= 3)
+    {
+        newList.Add(list[i]);
+    }
+}
